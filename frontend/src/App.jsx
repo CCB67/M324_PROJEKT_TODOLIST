4a573047f9a0b28195919bed7ad6fb7be4e9481d
Christopher <Carl-Christopher.Biebow@wiss-edu.ch>
2025-07-09 12:03:21 +0200

    added version and swagger support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import logo from "./assets/react.svg";
 
-// const API_BASE = 'http://localhost:4000/api/tasks';
+//const API_BASE = 'http://localhost:4000/api/tasks';
 //const API_BASE = `http://${window.location.hostname}:4000/api/tasks`;
-const API_BASE = '/api/tasks';
+const API_BASE = '/api/v1/tasks';
 
 function App() {
   const [taskDescription, setTaskDescription] = useState('');
